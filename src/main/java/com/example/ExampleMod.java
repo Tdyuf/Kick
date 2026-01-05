7e3c13c99297a1563c4c118ce1d8ddda8ac17f35
Tdyuf <150161991+Tdyuf@users.noreply.github.com>
2026-01-05 06:26:19 +0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,24 @@
-package com.example;
+package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        ClientSendMessageEvents.ALLOW_CHAT_MESSAGE.register(message -> {
+            if (message.equalsIgnoreCase(".kick")) {
+                MinecraftClient client = MinecraftClient.getInstance();
+                if (client.getNetworkHandler() != null) {
+                    client.getNetworkHandler().getConnection()
+                            .disconnect(Text.literal("You kicked yourself."));
+                }
+                return false;
+            }
+            return true;
+        });
+    }
 }
