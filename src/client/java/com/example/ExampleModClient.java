e4fb7ae1646306a6d9015c30010384f0c235851b
Tdyuf <150161991+Tdyuf@users.noreply.github.com>
2026-01-05 06:16:15 +0300

    Update ExampleModClient.java

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -1,10 +1,25 @@
-package com.example;
+package net.fabricmc.example;
 
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
 
-public class ExampleModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+public class ExampleMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        ClientSendMessageEvents.ALLOW_CHAT_MESSAGE.register(message -> {
+            if (message.equalsIgnoreCase(".kick")) {
+                MinecraftClient client = MinecraftClient.getInstance();
+                if (client.getNetworkHandler() != null) {
+                    client.getNetworkHandler().getConnection()
+                            .disconnect(Text.literal("You kicked yourself."));
+                }
+                return false;
+            }
+            return true;
+        });
+    }
 }
+
